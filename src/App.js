2cb217408d968fe1465fd1a024f45a06088e5602
WildCodeSchool <anelisynagano@gmail.com>
2020-10-16 21:51:07 +0100

    start repo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/** @jsx jsx */
+import React from "react";
+// import here jsx and css from @emotion/core library
+// import here styled from @emotion/styled library
+
+
+// create a variable list and apply the properties: display: flex; list-style-type: none;
+// create a variable title and apply the properties: color: #39424e;
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // add the attribute imported from the library to the html tags
+    <div>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+      <h1>Emotion!</h1>
+      <p>This is an example of Emotion</p>
     </div>
   );
 }
 
+// don't forget to wrap your component with the imported injectSheet!
 export default App;
